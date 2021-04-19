ed112b96ac8d0ae676671214988b5b42ee38a99b
NgToann <nguyentoan712@gmail.com>
2021-04-19 22:47:54 +0700

    initial

---
+++ dist/precache-manifest.cf11570e9b079781d70c1a7b93c72a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf38c5f511d5ec024682",
+    "url": "./static/css/main.87064543.chunk.css"
+  },
+  {
+    "revision": "bf38c5f511d5ec024682",
+    "url": "./static/js/main.bf38c5f5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "cba60c977c9843ef6a23",
+    "url": "./static/css/2.2dc353ef.chunk.css"
+  },
+  {
+    "revision": "cba60c977c9843ef6a23",
+    "url": "./static/js/2.cba60c97.chunk.js"
+  },
+  {
+    "revision": "cbc057d8ba65f9d2a335057569ccb1d2",
+    "url": "./index.html"
+  }
+];
